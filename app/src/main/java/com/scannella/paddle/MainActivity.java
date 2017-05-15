7dcb5ada00975a6dfa0e26066a6e3f57f0167dd1
scannella <scannella@bishopireton.org>
2017-05-15 09:45:07 -0400

    Here

--- app/src/main/java/com/scannella/paddle/MainActivity.java
+++ app/src/main/java/com/scannella/paddle/MainActivity.java
@@ -1,22 +1,48 @@
 package com.scannella.paddle;
 
 import android.app.Activity;
-import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Rect;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Point;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
-import android.view.View;
+import android.view.Menu;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(new CustomView(this));
-        }
+    float screenX = getResources().getDisplayMetrics().widthPixels;
+    float screenY = getResources().getDisplayMetrics().heightPixels;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Paint paint = new Paint();
+        paint.setColor(Color.GRAY);
+
+        Bitmap bg = Bitmap.createBitmap(Math.round(screenX), Math.round(screenY), Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(bg);
+
+
+
+
+        /*Point racketPosition = new Point();
+        racketPosition.x = Math.round(screenX) / 2;
+        racketPosition.y = Math.round(screenY) - 520;
+        int racketWidth = Math.round(screenX) / 8;
+        int racketHeight = 10;
+
+        canvas.drawRect(
+                racketPosition.x - (racketWidth / 2),
+                racketPosition.y - (racketHeight / 2),
+                racketPosition.x + (racketWidth / 2),
+                racketPosition.y + racketHeight, paint);
+
+    }*/
 
 }
